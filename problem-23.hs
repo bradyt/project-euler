a08595dedde26bb2c23848fa27d65c7fba6df500
Brady Trainor <brady@bradyt.com>
2016-07-02 10:12:30 -0700

    Before replacing reinsert with updateSums

--- problem-23.hs
+++ problem-23.hs
@@ -25,58 +25,61 @@ import Data.Map (Map)
 import qualified Data.Map as M
 import Math.NumberTheory.Primes.Factorisation
 import Control.Applicative
+import Data.Maybe (fromJust)
 
 isAbundant :: Integer -> Bool
 isAbundant n = (> 2*n) $ sum $ divisors n
 
-areAbundant :: [Integer] -> [Integer]
-areAbundant xs = filter isAbundant xs
-
 allAbundants :: [Integer]
-allAbundants = areAbundant [1..28123]
+allAbundants = filter isAbundant [1..28123]
 
-isDecomposable :: Integer -> Bool
-isDecomposable n = any (`elem` allAbundants) (map (n-) allAbundants)
-
-sumDecomposable :: [Integer] -> Integer
-sumDecomposable [] = 0
-sumDecomposable (x:xs)
-  | isDecomposable x = x + sumDecomposable xs
-  | otherwise        = sumDecomposable xs
-
-sieve :: [Integer] -> [Integer]
-sieve ys = sieve' ys M.empty
-  where
-    sieve' []     _     = []
-    sieve' (x:xs) table =
-      case (M.lookup x table) of
-        Nothing    -> x : sieve' xs (M.insert (x*x) [x] table)
-        Just facts -> sieve' xs (foldl reinsert (M.delete x table) facts)
-      where
-        reinsert table' prime = M.insertWith (++) (x+prime) [prime] table'
-
-abundantMap :: Map Integer Integer
+abundantMap :: Map Int Integer
 abundantMap = M.fromList $ zip [0..] allAbundants
 
-startingArray :: Map Integer [(Integer, Integer)]
-startingArray = M.fromList [ (2*a, [(a, a)]) | a <- allAbundants ]
+convertAbundantPtToSum :: (Int, Int) -> Integer
+convertAbundantPtToSum (x, y) = fromJust $
+  (+) <$> (M.lookup x abundantMap) <*> (M.lookup y abundantMap)
 
-problem23 = go 0 [1..28123] startingArray
-  where go :: Integer -> [Integer] -> Map Integer [(Integer, Integer)] -> Integer
-        go s []     _     = s
-        go s (x:xs) array =
-          case (M.lookup x array) of
-            Nothing     -> go (s+x) xs array
-            Just points -> go s xs (foldl
-                                    reinsert (M.delete x array) points)
-          where reinsert :: Map Integer [(Integer, Integer)]
-                         -> (Integer, Integer)
-                         -> Map Integer [(Integer, Integer)]
-                reinsert array' (p1, p2) =
-                  case (M.lookup (p2+1) abundantMap) of
-                    Just nextAbundant -> 
-                      M.insertWith (++) val [(p1, p2+1)] array'
-                      where val = 
-                              (abundantMap M.! p1) +
-                              nextAbundant
-                    Nothing -> array'
+abundantSumsArray :: Map Integer [(Int, Int)]
+abundantSumsArray = M.fromList [ (convertAbundantPtToSum (i, i), [(i, i)])
+                               | i <- [0..(length allAbundants) - 1] ]
+
+updateSums :: Map Integer [(Int, Int)] -> (Int, Int) -> Map Integer [(Int, Int)]
+updateSums array point
+  | snd point < (length allAbundants) =
+      M.insertWith (++) (convertAbundantPtToSum point) [point] array
+  | otherwise                = array
+
+-- removeSums :: [Integer] -> [Integer]
+-- removeSums xs = removeSumsRec xs abundantSumsArray
+
+-- removeSumsRec :: [Integer] -> Map Integer [(Integer, Integer)] -> [Integer]
+-- removeSumsRec [] _         = []
+-- removeSumsRec (x:xs) array =
+--   case (M.lookup x array) of
+--     Nothing     -> x:removeSumsRec xs array
+--     Just points -> removeSumsRec xs (foldl
+--                                         reinsert (M.delete x array) points)
+--   where reinsert :: Map Integer [(Integer, Integer)]
+--                  -> (Integer, Integer)
+--                  -> Map Integer [(Integer, Integer)]
+--         reinsert array' (p1, p2) =
+--           case (M.lookup (p2+1) abundantMap) of
+--             Just nextAbundant -> 
+--               M.insertWith (++) val [(p1, p2+1)] array'
+--               where val = 
+--                       (abundantMap M.! p1) +
+--                       nextAbundant
+--             Nothing -> array'
+
+-- sieve :: [Integer] -> [Integer]
+-- sieve ys = sieve' ys M.empty
+--   where
+--     sieve' []     _     = []
+--     sieve' (x:xs) table =
+--       case (M.lookup x table) of
+--         Nothing    -> x : sieve' xs (M.insert (x*x) [x] table)
+--         Just facts -> sieve' xs (foldl reinsert (M.delete x table) facts)
+--       where
+--         reinsert table' prime = M.insertWith (++) (x+prime) [prime] table'
+
