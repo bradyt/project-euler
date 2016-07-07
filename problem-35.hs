90157a2df078dbf1fc2eef0864d1e9f5a4f29f1b
Brady Trainor <brady@bradyt.com>
2016-07-07 06:43:32 -0700

    Commits 35, execute slow for k = 3

--- problem-35.hs
+++ problem-35.hs
@@ -9,3 +9,37 @@
 
 -- How many circular primes are there below one million?
 
+import Math.NumberTheory.Primes
+import Data.Char (digitToInt)
+import Data.Char (intToDigit)
+
+-- onlyThese :: [Int]
+onlyThese k = map fromIntegral $ takeWhile (<10^k) primes
+
+rotateList :: [a] -> [a]
+rotateList (x:xs) = xs ++ [x]
+
+-- intToList :: Integral a => a -> [a]
+-- intToList = (map digitToInt) . show
+
+-- listToInt :: Integral a => a -> [a]
+-- listToInt xs = read $ map intToDigit $ fromIntegral xs
+
+intToList :: Int -> [Int]
+intToList = (map digitToInt) . show
+
+listToInt :: [Int] -> Int
+listToInt xs = read $ map intToDigit xs
+
+rotateInt :: Int -> Int
+rotateInt = listToInt . rotateList . intToList
+
+-- checkIfCircularPrime :: Int -> Bool
+checkIfCircularPrime k p = go p p
+  where go n p
+          | m == p             = True
+          | m `elem` (onlyThese k) = go m p
+          | otherwise          = False
+          where m = rotateInt n
+
+problem35 k = length $ filter (checkIfCircularPrime k) $ onlyThese k
