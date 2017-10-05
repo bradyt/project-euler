de94d61fe174e371cc128671048b083f2ca2b028
Brady Trainor <mail@bradyt.com>
2017-10-04 20:51:06 -0700

    Saves p014.py with ~3s running time

--- python/p014.py
+++ python/p014.py
@@ -1,41 +1,55 @@
--- Longest Collatz sequence
--- Problem 14
+# Longest Collatz sequence
+# Problem 14
 
--- The following iterative sequence is defined for the set of positive
--- integers:
+# The following iterative sequence is defined for the set of positive
+# integers:
 
--- n → n/2 (n is even)
--- n → 3n + 1 (n is odd)
+# n → n/2 (n is even)
+# n → 3n + 1 (n is odd)
 
--- Using the rule above and starting with 13, we generate the
--- following sequence:
--- 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+# Using the rule above and starting with 13, we generate the
+# following sequence:
+# 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
 
--- It can be seen that this sequence (starting at 13 and finishing at
--- 1) contains 10 terms. Although it has not been proved yet (Collatz
--- Problem), it is thought that all starting numbers finish at 1.
+# It can be seen that this sequence (starting at 13 and finishing at
+# 1) contains 10 terms. Although it has not been proved yet (Collatz
+# Problem), it is thought that all starting numbers finish at 1.
 
--- Which starting number, under one million, produces the longest
--- chain?
+# Which starting number, under one million, produces the longest
+# chain?
 
--- NOTE: Once the chain starts the terms are allowed to go above one
--- million.
+# NOTE: Once the chain starts the terms are allowed to go above one
+# million.
 
-nextValue :: Int -> Int
-nextValue n = if even n
-              then n `div` 2
-              else 3*n+1
+from typing import Dict
 
-propogate :: Int -> Int
-propogate n = go (n, 1)
-  where go (1, l) = l
-        go (m, l) = go (nextValue m, l+1)
+def next_value(n: int) -> int:
+    return n // 2 if n % 2 == 0 else 3 * n + 1
 
-findMax :: Int -> Int
-findMax k = go (1, 1, 1)
-  where go (n, best, maxim)
-          | n >= k              = best
-          | propogate n > maxim = go (n+1, n, propogate n)
-          | otherwise           = go (n+1, best, maxim)
+def find_lengths() -> Dict[int, int]:
+    lengths = {1: 1}
+    def find_length_rec(n: int) -> int:
+        if n in lengths:
+            return lengths[n]
+        else:
+            length = find_length_rec(next_value(n)) + 1
+            lengths[n] = length
+            return length
+    for n in range(2, 10 ** 6):
+        if n not in lengths:
+            lengths[n] = find_length_rec(next_value(n)) + 1
+    return lengths
 
-main = print $ findMax 10^6
+def solution() -> int:
+    lengths = find_lengths()
+    k_max, v_max = 1, 1
+    for k, v in lengths.items():
+        if v > v_max:
+            k_max, v_max = k, v
+    return k_max
+
+def main() -> None:
+    print(solution())
+
+if __name__ == "__main__":
+    main()
