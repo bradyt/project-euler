05e27576ccde2522bc3f9236723300fa5591eb4b
Brady Trainor <mail@bradyt.com>
2017-10-06 11:56:21 -0700

    Commits last four problems descriptions below 15% difficulty level

---
+++ python/p493.py
@@ -0,0 +1,10 @@
+# Under The Rainbow
+# Problem 493
+# 70 colored balls are placed in an urn, 10 for each of the seven
+# rainbow colors.
+
+# What is the expected number of distinct colors in 20 randomly picked
+# balls?
+
+# Give your answer with nine digits after the decimal point
+# (a.bcdefghij).
